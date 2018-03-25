fe2bfe68dfe64fcd59ea1769504d10bb9da8f699
Yubo Zhi <normanzyb@gmail.com>
2018-03-25 20:20:22 +0100

    servmc: backup-git.sh: Fix git init problem when user full name is not set

--- scripts/backup-git.sh
+++ scripts/backup-git.sh
@@ -5,8 +5,10 @@ world="$(sed -ne 's/^level-name=\(.*\)$/\1/p' "$dir/server.properties")"
 
 cd "$dir/$world"
 if [ ! -e .git ]; then
+	name="$(grep $USER /etc/passwd | cut -d: -f5 | cut -d, -f1)"
+	[ -z "$name" ] && name="$(whoami)"
 	git init
-	git config user.name "$(grep $USER /etc/passwd | cut -d: -f5 | cut -d, -f1)"
+	git config user.name "$name"
 	git config user.email "$(whoami)@$(hostname)"
 	git config commit.gpgSign false
 	git config core.compression 9
