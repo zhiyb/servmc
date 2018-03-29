bdbfd829a39281b1d37f2b8cb38800be35e56033
YJBeetle <YJBeetle@gmail.com>
2018-03-29 22:24:48 +0800

    backup gui （only gui）

--- web/src/pages/backup.js
+++ web/src/pages/backup.js
@@ -1,11 +1,336 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, {
+	TableBody,
+	TableCell,
+	TableFooter,
+	TableHead,
+	TablePagination,
+	TableRow,
+	TableSortLabel,
+} from 'material-ui/Table';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import Checkbox from 'material-ui/Checkbox';
+import IconButton from 'material-ui/IconButton';
+import Tooltip from 'material-ui/Tooltip';
+import DeleteIcon from 'material-ui-icons/Delete';
+import FilterListIcon from 'material-ui-icons/FilterList';
+import { lighten } from 'material-ui/styles/colorManipulator';
 
-class backup extends Component {
+let counter = 0;
+function createData(a1, a2, a3, a4) {
+	counter += 1;
+	return { id: counter, a1, a2, a3, a4 };
+}
+
+const columnData = [
+	{ id: 'name', numeric: false, disablePadding: true, label: 'sha-1 ID' },
+	{ id: 'calories', numeric: true, disablePadding: false, label: '版本' },
+	{ id: 'fat', numeric: true, disablePadding: false, label: '备份日期' },
+	{ id: 'carbs', numeric: true, disablePadding: false, label: '操作' },
+];
+
+class EnhancedTableHead extends React.Component {
+	createSortHandler = property => event => {
+		this.props.onRequestSort(event, property);
+	};
+
 	render() {
+		const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+
 		return (
-			<h1>Backup</h1>
+			<TableHead>
+				<TableRow>
+					<TableCell padding="checkbox">
+						<Checkbox
+							indeterminate={numSelected > 0 && numSelected < rowCount}
+							checked={numSelected === rowCount}
+							onChange={onSelectAllClick}
+						/>
+					</TableCell>
+					{columnData.map(column => {
+						return (
+							<TableCell
+								key={column.id}
+								numeric={column.numeric}
+								padding={column.disablePadding ? 'none' : 'default'}
+								sortDirection={orderBy === column.id ? order : false}
+							>
+								<Tooltip
+									title="Sort"
+									placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+									enterDelay={300}
+								>
+									<TableSortLabel
+										active={orderBy === column.id}
+										direction={order}
+										onClick={this.createSortHandler(column.id)}
+									>
+										{column.label}
+									</TableSortLabel>
+								</Tooltip>
+							</TableCell>
+						);
+					}, this)}
+				</TableRow>
+			</TableHead>
 		);
 	}
 }
 
-export default backup;
+EnhancedTableHead.propTypes = {
+	numSelected: PropTypes.number.isRequired,
+	onRequestSort: PropTypes.func.isRequired,
+	onSelectAllClick: PropTypes.func.isRequired,
+	order: PropTypes.string.isRequired,
+	orderBy: PropTypes.string.isRequired,
+	rowCount: PropTypes.number.isRequired,
+};
+
+const toolbarStyles = theme => ({
+	root: {
+		paddingRight: theme.spacing.unit,
+	},
+	highlight:
+		theme.palette.type === 'light'
+			? {
+				color: theme.palette.secondary.main,
+				backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+			}
+			: {
+				color: theme.palette.text.primary,
+				backgroundColor: theme.palette.secondary.dark,
+			},
+	spacer: {
+		flex: '1 1 100%',
+	},
+	actions: {
+		color: theme.palette.text.secondary,
+	},
+	title: {
+		flex: '0 0 auto',
+	},
+});
+
+let EnhancedTableToolbar = props => {
+	const { numSelected, classes } = props;
+
+	return (
+		<Toolbar
+			className={classNames(classes.root, {
+				[classes.highlight]: numSelected > 0,
+			})}
+		>
+			<div className={classes.title}>
+				{numSelected > 0 ? (
+					<Typography color="inherit" variant="subheading">
+						{numSelected} selected
+          </Typography>
+				) : (
+						<Typography variant="title">备份列表</Typography>
+					)}
+			</div>
+			<div className={classes.spacer} />
+			<div className={classes.actions}>
+				{numSelected > 0 ? (
+					<Tooltip title="Delete">
+						<IconButton aria-label="Delete">
+							<DeleteIcon />
+						</IconButton>
+					</Tooltip>
+				) : (
+						<Tooltip title="Filter list">
+							<IconButton aria-label="Filter list">
+								<FilterListIcon />
+							</IconButton>
+						</Tooltip>
+					)}
+			</div>
+		</Toolbar>
+	);
+};
+
+EnhancedTableToolbar.propTypes = {
+	classes: PropTypes.object.isRequired,
+	numSelected: PropTypes.number.isRequired,
+};
+
+EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
+
+const styles = theme => ({
+	root: {
+		width: '100%',
+		marginTop: theme.spacing.unit * 3,
+	},
+	table: {
+		minWidth: 800,
+	},
+	tableWrapper: {
+		overflowX: 'auto',
+	},
+});
+
+//======================================
+
+class backup extends Component {
+	constructor(props, context) {
+		super(props, context);
+
+		this.state = {
+			order: 'asc',
+			orderBy: 'calories',
+			selected: [],
+			data: [
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+				createData('abb310a3d6854748e8893043c52f0eecbfb91cd5', "18w11a", "Thu Mar 29 20:47:50 2018 +0800"),
+			].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+			page: 0,
+			rowsPerPage: 5,
+		};
+	}
+
+	handleRequestSort = (event, property) => {
+		const orderBy = property;
+		let order = 'desc';
+
+		if (this.state.orderBy === property && this.state.order === 'desc') {
+			order = 'asc';
+		}
+
+		const data =
+			order === 'desc'
+				? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+				: this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
+
+		this.setState({ data, order, orderBy });
+	};
+
+	handleSelectAllClick = (event, checked) => {
+		if (checked) {
+			this.setState({ selected: this.state.data.map(n => n.id) });
+			return;
+		}
+		this.setState({ selected: [] });
+	};
+
+	handleClick = (event, id) => {
+		const { selected } = this.state;
+		const selectedIndex = selected.indexOf(id);
+		let newSelected = [];
+
+		if (selectedIndex === -1) {
+			newSelected = newSelected.concat(selected, id);
+		} else if (selectedIndex === 0) {
+			newSelected = newSelected.concat(selected.slice(1));
+		} else if (selectedIndex === selected.length - 1) {
+			newSelected = newSelected.concat(selected.slice(0, -1));
+		} else if (selectedIndex > 0) {
+			newSelected = newSelected.concat(
+				selected.slice(0, selectedIndex),
+				selected.slice(selectedIndex + 1),
+			);
+		}
+
+		this.setState({ selected: newSelected });
+	};
+
+	handleChangePage = (event, page) => {
+		this.setState({ page });
+	};
+
+	handleChangeRowsPerPage = event => {
+		this.setState({ rowsPerPage: event.target.value });
+	};
+
+	isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+	render() {
+		const { classes } = this.props;
+		const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+		const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+		return (
+			<Paper className={classes.root}>
+				<EnhancedTableToolbar numSelected={selected.length} />
+				<div className={classes.tableWrapper}>
+					<Table className={classes.table}>
+						<EnhancedTableHead
+							numSelected={selected.length}
+							order={order}
+							orderBy={orderBy}
+							onSelectAllClick={this.handleSelectAllClick}
+							onRequestSort={this.handleRequestSort}
+							rowCount={data.length}
+						/>
+						<TableBody>
+							{data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+								const isSelected = this.isSelected(n.id);
+								return (
+									<TableRow
+										hover
+										onClick={event => this.handleClick(event, n.id)}
+										role="checkbox"
+										aria-checked={isSelected}
+										tabIndex={-1}
+										key={n.id}
+										selected={isSelected}
+									>
+										<TableCell padding="checkbox">
+											<Checkbox checked={isSelected} />
+										</TableCell>
+										<TableCell padding="none">{n.a1}</TableCell>
+										<TableCell numeric>{n.a2}</TableCell>
+										<TableCell numeric>{n.a3}</TableCell>
+										{/* <TableCell numeric>{n.a4}</TableCell> */}
+										<TableCell numeric>回档</TableCell>
+									</TableRow>
+								);
+							})}
+							{emptyRows > 0 && (
+								<TableRow style={{ height: 49 * emptyRows }}>
+									<TableCell colSpan={6} />
+								</TableRow>
+							)}
+						</TableBody>
+						<TableFooter>
+							<TableRow>
+								<TablePagination
+									colSpan={6}
+									count={data.length}
+									rowsPerPage={rowsPerPage}
+									page={page}
+									backIconButtonProps={{
+										'aria-label': 'Previous Page',
+									}}
+									nextIconButtonProps={{
+										'aria-label': 'Next Page',
+									}}
+									onChangePage={this.handleChangePage}
+									onChangeRowsPerPage={this.handleChangeRowsPerPage}
+									labelRowsPerPage="每页行数："
+									labelDisplayedRows={({ from, to, count }) => `${from}-${to} 共 ${count}`}
+								/>
+							</TableRow>
+						</TableFooter>
+					</Table>
+				</div>
+			</Paper>
+		);
+	}
+	static propTypes = {
+		classes: PropTypes.object.isRequired,
+	};
+}
+
+export default withStyles(styles)(backup);
